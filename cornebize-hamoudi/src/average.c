2cebc7b3965ac71a55fcc35d81d35d217ae5826e
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-12-06 16:06:39 +0100

    Input/Output.

--- cornebize-hamoudi/src/average.c
+++ cornebize-hamoudi/src/average.c
@@ -5,7 +5,52 @@
 #include "data.h"
 
 #define ONLY 0
+#define TAG 0
 
+void compute(Process process) {
+    fprintf(stderr,"Process %d is computing...\n",process->myid) ;
+    process->nbIter = process->currentIter ;
+}
+
+void io(Process process) {
+    int i, buffio[3] = {0,0,0} ;
+    double content ;
+    MPI_Status stat;
+    while(1) {
+        if(process->myid == ONLY) {
+            for(i = 0 ; i < 3 ; i++) {
+                if(scanf("%d",&buffio[i])==EOF) {
+                    buffio[0] = -1 ;
+                    break ;
+                }
+            }
+            if(scanf("%lf",&content)==EOF)
+                buffio[0] = -1 ;
+        }
+        MPI_Bcast(buffio, 3, MPI_INT, ONLY, MPI_COMM_WORLD) ;
+        MPI_Bcast(&content, 1, MPI_DOUBLE, ONLY, MPI_COMM_WORLD) ;
+        if(buffio[0] == -1)
+            break ;
+        if(buffio[0] == 0) {
+            if(setCellValue(process->automata,buffio[1],buffio[2],content))
+                fprintf(stderr,"Process %d: set cell (%d,%d) to value %lf\n",process->myid,buffio[1],buffio[2],content) ;
+        }
+        if(buffio[0] == 2) {
+            while(process->nbIter > process->currentIter) {
+                compute(process) ;
+            }
+            content = getCell(process->automata,buffio[1],buffio[2]) ;
+            if(!isnan(content) && !process->myid == ONLY) {
+                MPI_Send(&content, 1, MPI_DOUBLE, ONLY, TAG, MPI_COMM_WORLD);
+            }
+            if(isnan(content) && process->myid == ONLY) {
+                MPI_Recv(&content, 1, MPI_DOUBLE, MPI_ANY_SOURCE, TAG, MPI_COMM_WORLD,&stat);
+            }
+            if(process->myid == ONLY)
+                printf("%lf\n",content) ;
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     MPI_Init(&argc,&argv) ;
@@ -26,10 +71,7 @@ int main(int argc, char **argv) {
     MPI_Bcast(&nbiter, 1, MPI_INT, ONLY, MPI_COMM_WORLD) ;
     /* MPI programs start with MPI_Init; all 'N' processes exist thereafter */
     process = initProcess(myid,nbproc,width,height,p,nbiter) ;
-
-    if(process->myid==ONLY)
-        printf("# %d %d\n",process->gridHeight,process->gridWidth) ;
-    printf("    %d: %d %d\n",process->myid,process->myrow,process->mycol) ;
+    io(process);
     delProcess(process);
     MPI_Finalize() ;
     return 0 ;

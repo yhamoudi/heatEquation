600405832da94aec84fc5e0e71eac8965eb67d8c
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-12-07 12:34:54 +0100

    Update script.

--- cornebize-hamoudi/check.sh
+++ cornebize-hamoudi/check.sh
@@ -7,6 +7,11 @@ mpirun -n 1 average < $1 > tmp1.out
 for i in {2..10}
 do
     mpirun -n $i average < $1 > tmp2.out
+    if [ $? -ne 0 ]
+    then
+        echo "Error for n="$i
+        exit
+    fi
     diff tmp1.out tmp2.out
     if [ $? -ne 0 ]
     then

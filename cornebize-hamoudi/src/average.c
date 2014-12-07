0c5006c6641a70ae6c79dd3667bf92a976e7ed09
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-12-07 21:51:59 +0100

    Finally found the silly malloc bug...

--- cornebize-hamoudi/src/average.c
+++ cornebize-hamoudi/src/average.c
@@ -9,6 +9,8 @@
 
 #define ONLY 0
 #define TAG 0
+#define max(a,b) (a>b ? a : b)
+#define min(a,b) (a>b ? b : a)
 
 /* Compute in a distributed fashion one step of the delta function. */
 void compute(Process process, double **buff) {
@@ -80,7 +82,7 @@ void inputOutput(Process process) {
     double content ;
     double *buffcolumns[8] ; /* buffers for messages */
     for(i=0 ; i < 8 ; i++) {
-        buffcolumns[i] = (double*) malloc(process->automata->height*sizeof(double)) ;
+        buffcolumns[i] = (double*) malloc(max(process->automata->height,process->automata->width)*sizeof(double)) ;
         assert(buffcolumns[i]) ;
     }
     MPI_Status stat;
